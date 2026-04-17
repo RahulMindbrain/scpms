f2e0ca108c38098d21388b57bb3a82c67afd4457
anjz24 <anjali.s.mindbrain@gmail.com>
2026-04-17 05:56:03 +0530

    feat: initialize socket.io client configuration with disabled auto-connect

--- Frontend/src/socket/socket.config.ts
+++ Frontend/src/socket/socket.config.ts
@@ -1,9 +1,8 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://127.0.0.1:3030";
+const SOCKET_URL = "http://localhost:3030";
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
-  transports: ["websocket", "polling"],
 });
