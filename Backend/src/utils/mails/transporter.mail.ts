36b359ad6f9e112e036dea00ca0c01ec5624c7cd
RahulMindbrain <rahul.mindbrain@gmail.com>
2026-05-13 17:00:12 +0530

    fix: nodemailer transport typings

--- Backend/src/utils/mails/transporter.mail.ts
+++ Backend/src/utils/mails/transporter.mail.ts
@@ -1,19 +1,24 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-export const transporter = nodemailer.createTransport({
+const config: SMTPTransport.Options = {
   host: process.env.MAIL_HOST,
+
   port: Number(process.env.MAIL_PORT),
+
   secure: process.env.MAIL_SECURE === "true",
-  tls: {
-    family: 4,
-  },
+
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.MAIL_USER!,
+    pass: process.env.MAIL_PASS!,
   },
+
   logger: true,
+
   debug: true,
-});
+};
+
+export const transporter = nodemailer.createTransport(config);
 
 type MailOptions = {
   to: string | string[];
@@ -25,14 +30,18 @@ export const sendMail = async ({ to, subject, html }: MailOptions) => {
   try {
     const info = await transporter.sendMail({
       from: `"Smart Campus Placement Portal" <${process.env.MAIL_USER}>`,
+
       to,
+
       subject,
+
       html,
     });
 
     return info;
   } catch (error) {
     console.error("Mail Error:", error);
+
     throw error;
   }
 };
